cec4e89f900604207230ef27b7a30cff883101bb
Shadi Abu Jaber <shadiabo2@m.telhai.ac.il>
2025-12-10 15:19:18 +0200

    New Project Design Patterns Created

---
+++ Telhai.CS.Csharp.DesignPatterns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Telhai.CS.Csharp.DesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
